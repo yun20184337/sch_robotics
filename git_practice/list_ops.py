a725366fbba0a04412bd848c0098f5c6c4d05d20
gyeongmin.yun <summer2164@naver.com>
2021-11-15 16:00:59 -1000

    change main to use list ops

--- git_practice/list_ops.py
+++ git_practice/list_ops.py
@@ -10,3 +10,15 @@ def subtract(foo, bar):
     for f, b in zip(foo, bar):
         out.append(f - b)
     return out
+# 아래쪽에 추가
+def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
